ab63bf266108b28ebb3daa52d60e02da6fb57f5c
Kyle Isom <coder@kyleisom.net>
2010-12-24 06:38:48 +0000

    Start rawtcp_handshake.
    rawtcp_handshake will handbuild the packets required to initiate and
    complete a TCP handshake, i.e. generate a SYN and a PSH/ACK.

---
+++ clients/rawtcp_handshake.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+use Net::RawIP;
+use Time::localtime;
+
+my $src = $ARGV[0] or &usage();
+my $dst = $ARGV[1] or &usage();
+my $sp  = int(rand(6000)) + 1024;
+my $dp  = 4141;
+my $mss = pack('n', 16396);
+my $seq = 0;
+
+
+print "[+] will connect from $src:$sp to $dst:$dp\n";
+print "[+] building SYN packet...\n";
+my $syn_pkt = Net::RawIP->new({
+                            ip => {
+                                saddr   => $src,
+                                daddr   => $dst
+                            },
+                            tcp => {
+                                source  => $sp,
+                                dest    => $dp,
+                                seq     => $seq,
+                                syn     => 1,
+                                window  => 32792, 
+                            }
+                        });
+
+$syn_pkt->optset(
+                tcp => {
+                    type => [ 2, 4, 8 ],	# set MSS and SACK
+                    data => [ $mss, '', pack('N', localtime()) ],
+                }
+            );
+print "[+] sending packet...\n";
+$syn_pkt->send( );              # send one packet without delay
+print "[+] finished!\n";
+exit 0;
+
+sub usage( ) {
+	die "usage: $0 <src> <dst>\n";
+}
+
+
