6bafb8c30ad3303ce3cf656393876adf17304343
Kyle Isom <sadm@kyleisom.net>
2010-12-27 16:17:59 -0700

    Working packet processor, need to get ACK recognition working.

--- clients/rawtcp_handshake.pl
+++ clients/rawtcp_handshake.pl
@@ -22,14 +22,17 @@ my $ack_seq     = 0;
 
 # PCAP SETUP
 my $err         = '';
-my $dev         = Net::Pcap::lookupdev(\$err);
+#my $dev         = Net::Pcap::lookupdev(\$err);
 my $dev         = "lo";
-my $filter_str  = 'tcp port $dp';
+my $filter_str  = "ip and tcp";
 my $filter      = '';
 my $snaplen     = 1600 ;
-my $timeout     = 5;
+my $timeout     = 0;
 my $pcap        = '';
 my $sent        = 0;
+my ($net, $mask)= '';
+Net::Pcap::lookupnet($dev, \$net, \$mask, \$err);
+printf "net: %x\tmask: %x\n", $net, $mask;
 
 
 
@@ -80,6 +83,10 @@ $ack_pkt->optset(
 
 # set up pcap
 $pcap = Net::Pcap::pcap_open_live($dev, $snaplen, 1, $timeout, \$err);
+if (Net::Pcap::compile($pcap, \$filter, $filter_str, 0, $mask)) {
+    print "[!] error compiling capture filter!\n";
+    exit 1;
+}
 Net::Pcap::pcap_setnonblock($pcap, 1, \$err);
 #print "got ack $ack_seq\n";
 
@@ -87,6 +94,7 @@ while (! $ack_seq) {
     if (! $sent) {
         print "[+] sending SYN...\n";
         $syn_pkt->send( );
+        $sent = 1;
     }
     Net::Pcap::pcap_dispatch($pcap, 1, \&load_ack_seq, '');
 }
@@ -113,9 +121,30 @@ sub load_ack_seq {
     my ($data, $hdr, $pkt) = @_ ;
 
     if (!$pkt || !defined($hdr) || !defined($pkt)) {
-        print "[!] no packets captured!\n";
+        print "[!] malformed packet!\n";
     }
 
-    $pkt = NetPacket::TCP->decode(NetPacket::IP->
-              strip(NetPacket::Ethernet->strip($pkt)));
+    my $eth     = NetPacket::Ethernet->decode($pkt) ;
+    my $ip      = NetPacket::IP->decode($eth->{data}) ;
+    my $tcp     = NetPacket::IP->decode($ip->{data}) ;
+
+    my $read_seq = $tcp->{seqnum} ;
+    print "SEQ: $read_seq\n";
+    if (defined $read_seq && $read_seq) {
+        if ($read_seq == $syn_seq + 1) {
+            print "[+] received ACK to our SYN...\n" ;
+            $ack_seq = $tcp->{acknum} ;
+            print "[+] ACK response has ACK number $ack_seq...\n";
+        }
+        else {
+            print "[+] ignoring TCP packet from $ip->{src_ip}";
+            print ":$tcp->{src_port} to $ip->{dest_ip}";
+            print ":$tcp->{dest_port}...\n";
+        }
+    }
+    else {
+        print "[*] ignoring packet from $ip->{src_ip} to ";
+        print "$ip->{dest_ip}...\n";
+    }   
+
 }
